562f839a1effc9651c3031b9f821fa6b18f76d47
Songurov Fiodor <songurov1@gmail.com>
2013-08-30 18:33:04 +0300

    get and set

--- Test-1/Test-1/Program.cs
+++ Test-1/Test-1/Program.cs
@@ -10,7 +10,30 @@ namespace Test_1
     {
         static void Main(string[] args)
         {
+            Commodity cmd = new Commodity();
+            cmd.name = "Jacobs";
+            
+            cmd.Cost = 12.3f;
+            Console.WriteLine("Suma in parte pentru un cofe: "+cmd.Cost);
+            Console.WriteLine("Suma cafe '"+cmd.name+"' este: " + cmd.GetCost());
+            Console.ReadKey();
+        }
+    }
+    class Commodity
+    {
+        public string name;
+        private float cost;
+        public float procente = 34.5f;
 
+        public float Cost
+        {
+            get { return cost; }
+            set { cost = value; }
+        }
+
+        public float GetCost()
+        {
+            return (Cost*2)*procente/100;
         }
     }
 }
