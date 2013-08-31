2801897f09b26ba4535a48589922a5906811a117
Songurov Fiodor <songurov1@gmail.com>
2013-08-31 15:25:11 +0300

    Finish Test - 2

--- Test-2/Test-2/Program.cs
+++ Test-2/Test-2/Program.cs
@@ -10,21 +10,47 @@ namespace Test_2
     {
         static void Main(string[] args)
         {
+            Frist frist = new Frist();
+            Second second = new Second();
+            MyClass myClass = new MyClass();
+
+            
+            Console.WriteLine("Frist = {0}, Second = {1} , MyClass = {2} ,{3} - {4} - {5} -- {6}", 
+                                frist.Fs,second.Fs,myClass.Fs,frist.ShowMessage(),second.ShowMessage(),frist.Ff,second.Modific());
+            Console.ReadKey();
+
         }
     }
 
     class Frist
     {
-        public int Fs;
+        public int Fs = 23;
+        private string ff;
+
+        public string Ff
+        {
+            get { return ff; }
+            set { ff = value; }
+        }
+
+        public string ShowMessage()
+        {
+            return Ff = "Plombir";
+        }
     }
 
     class Second: Frist
     {
         public int Ss;
+
+        public string Modific()
+        {
+            return Ff = "Ion";
+        }
     }
 
     class MyClass:Second
     {
-         
+        public int Mc;
     }
 }
