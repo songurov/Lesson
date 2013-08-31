abf7f3d0e0f0e1f47372caed9dc02a117eb6e083
Songurov Fiodor <songurov1@gmail.com>
2013-08-31 16:44:33 +0300

    Finish Test 3

--- Test-3/Test-3/Program.cs
+++ Test-3/Test-3/Program.cs
@@ -10,6 +10,36 @@ namespace Test_3
     {
         static void Main(string[] args)
         {
+            SportCar sc = new SportCar("Rosu",2000,"Ford Focus",true,6);
+
+            Console.WriteLine("Culoarea masini este {0} are o greutate de {1} Kg, se numeste {2} are {3} trepte si {4}",sc.color,sc.height,sc.name,sc.transmision,sc.complet_drive);
+            Console.ReadLine();
+        }
+    }
+
+    class Car
+    {
+        public string color;
+        public int height;
+        public string name;
+
+        public Car(string color , int height, string name)
+        {
+            this.color = color;
+            this.height = height;
+            this.name = name;
+        }
+    }
+
+    class SportCar : Car
+    {
+        public bool complet_drive;
+        public int transmision;
+
+        public SportCar(string color,int height,string name,bool complet,int transmision):base(color,height,name)
+        {
+            this.complet_drive = complet;
+            this.transmision = transmision;
         }
     }
 }
