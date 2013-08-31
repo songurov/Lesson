4b97383311167267d2361ba8b01f73201368f85b
Songurov Fiodor <songurov1@gmail.com>
2013-08-31 15:05:15 +0300

    New Lesson

---
+++ Test-2/Test-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Frist
+    {
+        public int Fs;
+    }
+
+    class Second: Frist
+    {
+        public int Ss;
+    }
+
+    class MyClass:Second
+    {
+         
+    }
+}
