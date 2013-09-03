33cea202ab1f7f232a4be944854ddbbf195cd14c
Songurov <songurov1@gmail.com>
2013-09-03 15:00:09 +0400

    sealed

---
+++ Test-7/Test-7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class A
+    {
+        public virtual void M()
+        {
+            Console.WriteLine("Test-7");
+            Console.ReadLine();
+        }
+    }
+
+    sealed class B:A
+    {
+        public override void M()
+        {
+            
+        }
+    }
+}
